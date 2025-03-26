38ac53085dea2c962867d8926c7613740a648ea3
Daisferry <vlad.daisferry10@gmail.com>
2025-03-26 21:19:35 +0200

    JS Update
    added js files and separated code for better management

---
+++ play/play.js
@@ -0,0 +1,30 @@
+const statusText = document.getElementById('status');
+    const cancelButton = document.getElementById('cancel-button');
+
+    
+    setTimeout(() => 
+    {
+      statusText.textContent = "Opponent found! Preparing match...";
+      setTimeout(() => 
+      {
+        statusText.textContent = "Match starting in 3... 2... 1...";
+        setTimeout(() => 
+        {
+          statusText.textContent = "Match found, redirecting shortly. Good luck!";
+          cancelButton.style.display = 'none'; 
+          setTimeout(() => 
+          {
+            window.location.href="../main/main.html";
+          }, 3000)
+        }, 3000);
+      }, 2000);
+    }, 5000);
+
+    
+    cancelButton.addEventListener('click', () => {
+      statusText.textContent = "Scanning canceled.";
+      cancelButton.style.display = 'none'; 
+      setTimeout(() => {
+        window.location.href="../main/main.html";
+      }, 1000)
+    });
---
+++ sign_up/sign_up.js
@@ -0,0 +1,30 @@
+const loginForm = document.getElementById('login-form');
+    const registerForm = document.getElementById('register-form');
+    const registerLink = document.getElementById('register-link');
+    const loginLink = document.getElementById('login-link');
+    const loginTextLink = document.getElementById('login-text-link');
+
+    registerLink.addEventListener('click', (e) => {
+      e.preventDefault();
+      loginForm.style.display = 'none';
+      registerForm.style.display = 'block';
+      loginLink.style.display = 'block';
+    });
+
+    loginTextLink.addEventListener('click', (e) => {
+      e.preventDefault();
+      registerForm.style.display = 'none';
+      loginForm.style.display = 'block';
+      loginLink.style.display = 'none';
+    });
+
+
+    loginForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      alert('Login successful! (Nu, nu ti-am furat datele.. inca.)');
+    });
+
+    registerForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      alert('Registration successful! (Nu, nu ti-am furat datele.. inca.)');
+    });
